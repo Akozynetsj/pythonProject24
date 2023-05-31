bb16d13b1fe358bd8d21b44a9065fd77dcb86e7d
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-05-31 20:59:47 +0300

    ex 3

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import math
+
+
+def calculate_hexagonal_pyramid_volume():
+    a = float(input("Введіть довжину сторони основи (a): "))
+    h = float(input("Введіть висоту піраміди (h): "))
+    volume = (3 * math.sqrt(3) / 2) * a ** 2 * h
+    return volume
+pyramid_volume = calculate_hexagonal_pyramid_volume()
+print(f"Об'єм шестикутної піраміди: {pyramid_volume}")
 
